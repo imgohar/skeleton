70f7a90a9f31a188cc25e710b2ab80768159bac8
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2021-03-06 15:53:47 +0500

    added auth

--- resources/views/layout/partials/extras/_topbar.blade.php
+++ resources/views/layout/partials/extras/_topbar.blade.php
@@ -128,10 +128,10 @@
         @if (config('layout.extras.user.layout') == 'offcanvas')
             <div class="topbar-item">
                 <div class="btn btn-icon w-auto btn-clean d-flex align-items-center btn-lg px-2" id="kt_quick_user_toggle">
-                    <span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi,</span>
-                    <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Sean</span>
+                    <span class="text-muted font-weight-bold font-size-base d-none d-md-inline mr-1">Hi 2,</span>
+                    <span class="text-dark-50 font-weight-bolder font-size-base d-none d-md-inline mr-3">Gopal</span>
                     <span class="symbol symbol-35 symbol-light-success">
-                        <span class="symbol-label font-size-h5 font-weight-bold">S</span>
+                        <span class="symbol-label font-size-h5 font-weight-bold">G</span>
                     </span>
                 </div>
             </div>
