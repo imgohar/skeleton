52fdfe62584f1464439d02623599ad0267091224
Gohar Ashraf <gohar.ashraf78611@gmail.com>
2021-03-07 09:14:33 +0500

    fixed icons

--- resources/views/layout/partials/extras/dropdown/_user.blade.php
+++ resources/views/layout/partials/extras/dropdown/_user.blade.php
@@ -17,11 +17,14 @@
         <div class="d-flex align-items-center mr-2">
             {{-- Symbol --}}
             <div class="symbol bg-white-o-15 mr-3">
-                <span class="symbol-label text-success font-weight-bold font-size-h4">S</span>
+                @php
+                    $name = Auth::user()->name;
+                @endphp
+                <span class="symbol-label text-success font-weight-bold font-size-h4">{{$name[0]}}</span>
             </div>
 
             {{-- Text --}}
-            <div class="text-white m-0 flex-grow-1 mr-3 font-size-h5">Sean Stone</div>
+            <div class="text-white m-0 flex-grow-1 mr-3 font-size-h5">{{Auth::user()->name}}</div>
         </div>
         <span class="label label-success label-lg font-weight-bold label-inline">3 messages</span>
     </div>
